e598c6951a20eb7e6f899e1baa7ce4337070a4a7
0sugo <josugo26@gmail.com>
2023-06-06 13:55:17 +0300

    Add query to select all animals that names are not Gabumon

--- queries.sql
+++ queries.sql
@@ -7,4 +7,5 @@ SELECT name FROM animals WHERE neutered=B'1' AND escape_attempts<3;
 SELECT date_of_birth  FROM animals WHERE name IN ('Agumon','Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=B'1';
+SELECT * FROM animals WHERE name!='Gabumon';
 
