8f34d2725f20d5875a2629a0811977616c34384f
0sugo <josugo26@gmail.com>
2023-06-06 11:41:20 +0300

    Create new table with columns

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+CREATE TABLE animals(
+    id int PRIMARY KEY ,
+    name VARCHAR NOT NULL,
+    date_of_birth date  NOT NULL ,
+    escape_attempts int NOT NULL,
+    neutered bit NOT NULL,
+    weight_kg decimal(10,2) NOT NULL
+);
