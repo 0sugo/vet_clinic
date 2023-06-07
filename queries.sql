eca271c02449fe749f5d0b04abe0ad1d15120546
0sugo <josugo26@gmail.com>
2023-06-07 15:04:45 +0300

    Add Transaction 2

--- queries.sql
+++ queries.sql
@@ -12,3 +12,12 @@ BEGIN;
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 UPDATE animals SET species = 'unspecified';
 ROLLBACK;
+
+-- TRANSACTION 2
+BEGIN;
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals SET species = 'pokeman' WHERE species !='digimon'OR species IS NULL;
+-- Commit the transaction.
+COMMIT;
