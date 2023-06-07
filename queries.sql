9d9c99750f1da1032bbea36957275dfd5f2fa7ed
0sugo <josugo26@gmail.com>
2023-06-07 15:37:44 +0300

    Update correct date format

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM animals WHERE name LIKE '%mon%';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/31';
 SELECT name FROM animals WHERE neutered=B'1' AND escape_attempts<3;
 SELECT date_of_birth  FROM animals WHERE name IN ('Agumon','Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
@@ -31,7 +31,7 @@ ROLLBACK;
 -- TRANSACTION 4
 BEGIN;
 -- Delete all animals born after Jan 1st, 2022.
-DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+DELETE FROM animals WHERE date_of_birth >= '2022/01/01';
 -- Create a savepoint for the transaction.
 SAVEPOINT svp1;
 -- Update all animals' weight to be their weight multiplied by -1.
@@ -57,4 +57,4 @@ SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 -- What is the minimum and maximum weight of each type of animal?
 SELECT species ,MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species,AVG(escape_attempts),date_of_birth FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species ,date_of_birth;
+SELECT species,AVG(escape_attempts),date_of_birth FROM animals WHERE date_of_birth BETWEEN '1990/01/01' AND '2000/12/31' GROUP BY species ,date_of_birth;
