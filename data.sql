5c74a2df09d42efa458f62ebeb70d76792fdef1d
0sugo <josugo26@gmail.com>
2023-06-08 11:33:05 +0300

    Add data to species table

--- data.sql
+++ data.sql
@@ -8,3 +8,6 @@ INSERT INTO animals (name , date_of_birth , escape_attempts , neutered , weight_
 
 -- Insert data to owners
 INSERT INTO owners(full_name , age) VALUES ('Sam Smith' , 34),('Jennifer Orwell' , 19),('Bob' , 45),('Melody Pond' , 77),('Dean Winchester' , 14),('Jodie Whittaker' , 38);
+
+-- Insert data to species table
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon'); 
