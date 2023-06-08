b4145ea7f3366dd3c986d0e61d9557d533add05b
0sugo <josugo26@gmail.com>
2023-06-09 00:22:01 +0300

    Add new specialisations table for many to many rltnships

--- schema.sql
+++ schema.sql
@@ -29,3 +29,6 @@ ALTER TABLE animals ADD CONSTRAINT fk_2 FOREIGN KEY (owner_id) REFERENCES owners
 -- Create a table named vets with (id,name,age) columns:
 CREATE TABLE vets (id SERIAL PRIMARY KEY , name VARCHAR NOT NULL,age INT NOT NULL,date_of_graduation DATE NOT NULL);
 
+-- many-to-many relationship between the tables species and vets
+CREATE TABLE specializations (vet_id INT NOT NULL, species_id INT NOT NULL, CONSTRAINT fk_vet FOREIGN KEY (vet_id) REFERENCES vets(id),CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id));
+
