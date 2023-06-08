476c2871a4be64a6d7351bc9fc89cb12b7872aba
0sugo <josugo26@gmail.com>
2023-06-08 14:48:41 +0300

    Modify animals to include owner_id

--- data.sql
+++ data.sql
@@ -14,3 +14,6 @@ INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
 
 -- Modify yoserted animals so it includes thur ine species_id value
 UPDATE animals SET species_id =CASE WHEN name LIKE '%mon%' THEN (SELECT id FROM species WHERE name ='Digimon' )ELSE (SELECT id FROM species WHERE name='Pokemon') END;
+
+-- Modify your inserted animals to include owner information (owner_id):
+UPDATE animals SET owner_id = CASE WHEN name= 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')WHEN name IN('Gabumon' , 'Pikachu')THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')WHEN name IN('Devimon','Plantmon') THEN (SELECT id FROM owners WHERE full_name='Bob')WHEN name IN('Charmander', 'Squirtle','Blossom')THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')ELSE (SELECT id FROM owners WHERE full_name = 'Dean Winchester') END;
