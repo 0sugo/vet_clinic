fcd49c9750db7d5638737344c054dd286eb77db9
0sugo <josugo26@gmail.com>
2023-06-09 09:54:43 +0300

    Insert the following data for specialties

--- schema.sql
+++ schema.sql
@@ -37,3 +37,11 @@ CREATE TABLE visits(vets_id INT NOT NULL, animals_id INT NOT NULL , CONSTRAINT f
 
 -- Insert the following data for vets:
 INSERT INTO vets (name,age,date_of_graduation) VALUES ('William Tatcher' , 45 , '2000-04-23'),('Maisy Smith' , 26 , '2019-01-17'),('Stephanie Mendez' , 64 , '1981-05-04'),('Jack Harkness' , 38 , '2008-06-08');
+
+-- Insert the following data for specialties:
+INSERT INTO specializations (vet_id, species_id)
+VALUES
+  ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon')),
+  ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon')),
+  ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+  ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
